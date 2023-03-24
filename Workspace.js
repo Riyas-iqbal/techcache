6557616e31747efc23c0c05dcebf2eb88ff57082
Riyas Iqbal <riyasiqbal10@gmail.com>
2023-03-24 11:19:34 +0530

    Promise Sample

--- Workspace.js
+++ Workspace.js
@@ -1,33 +1,10 @@
-# Promises
-
-**What are Promises?**
-
-Promises are objects which are used to perform asynchronous operations. They are just like placeholders to store a future value that will be returned after some time. They contain two properties: PromiseState and PromiseResult.
-
-
- **Importance of Promises:**
-
-* Promises can help us to write trust worthy code.
-* Promise is immutable
-* Promises are used to solve the problems of callbacks like inversion of control and callback hell.
-* They give us the result prompt in three states:
-    - Pending
-    - Fulfilled
-    - Rejected
-* We can attach function to promise object and retrieve its value unlike callbacks no need to pass the function.
-* Nesting can be done in Promises and with the help of that we can return the values in each individual chain.
-
-
-
-```js
 let data = true;
 
 asyncTask()
     .then((data) => asyncTask2(data))
     .then((data) => asyncTask3(data))
     .then((data) => asyncTask4(data))
-    .catch((error)=> console.log('error',error))
-
+    .catch((error) => console.log('error', error))
 
 function asyncTask() {
     return new Promise((resolve, reject) => {
@@ -72,4 +49,3 @@ function asyncTask4(newData) {
         }
     })
 }
-```
